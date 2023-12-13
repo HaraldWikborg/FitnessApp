b924459d17c3bc1992f299b252dc529a638df3df
Harald W <harald.wikborg@gmail.com>
2023-12-13 18:56:45 +0100

    Set up react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './components/home/home';
+import Nav from './components/nav/Nav';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.css';
+
+function Nav() {
+    return (
+        <>
+        <p>Navbar</p>
+        </>
+    )
+}
+
+export default Nav;
