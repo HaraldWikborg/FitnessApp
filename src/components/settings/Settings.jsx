371daae7a176001032cfd5157cf435d92e2218f1
Harald W <harald.wikborg@gmail.com>
2024-02-15 23:59:48 +0100

    Add settings page created settings page with css

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,12 +1,36 @@
-import './Settings.css'
+import React from "react";
+import "./Settings.css";
 
-function Settings () {
+function Settings() {
+  const handleChangeProfile = () => {
+    // Logic to change profile information
+  };
 
-    return (
-        <>
-        
-        </>
-    )
+  const handleLogout = () => {
+    // Logic to log out the user
+  };
+
+  const handleDeleteUser = () => {
+    // Logic to delete the user
+  };
+
+  return (
+    <div className="settingPage">
+      <h1>Settings</h1>
+      <div className="profileInfo">
+        <h3>Profile Information</h3>
+        <p>Name: John Doe</p>
+        <p>Email: </p>
+      </div>
+      <button onClick={handleChangeProfile}>Change Profile Information</button>
+      <button onClick={handleLogout} className="logOut">
+        Log Out
+      </button>
+      <button onClick={handleDeleteUser} className="deleteUser">
+        Delete User
+      </button>
+    </div>
+  );
 }
 
 export default Settings;
